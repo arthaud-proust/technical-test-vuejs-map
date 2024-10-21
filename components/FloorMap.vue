d0a44ef7e5746ceab11de53d2c1d382949a3aee5
Arthaud <arthaudp33@gmail.com>
2024-10-21 14:44:17 +0200

    :sparkles: Zoom on marker
    Struggled with the defineExpose because it was after an await
    Explanation : https://eslint.vuejs.org/rules/no-expose-after-await

--- components/FloorMap.vue
+++ components/FloorMap.vue
@@ -12,14 +12,51 @@ const props = defineProps<{
     markers: Array<Marker>
 }>();
 
+const map = ref();
+const mapContainer = ref<HTMLDivElement>();
+
+const zoomOnMarker = (marker: Marker) => {
+    if (!marker) return
+
+    map.value.setView([marker.x, marker.y], 1)
+}
+
+defineExpose({
+    zoomOnMarker,
+})
+
 const floorPlanImage = await getImageSizes(props.floorPlanUrl)
 const floorPlanImageBounds = [
     [0, 0],
     [floorPlanImage.height, floorPlanImage.width]
 ] as L.LatLngBoundsExpression
 
-const mapContainer = ref<HTMLDivElement>();
-const map = ref();
+const leafletMarkers = ref<Record<number, L.Marker>>({})
+const updateMarkers = (markers: Array<Marker>) => {
+    const markerIds = markers.map(marker => marker.id)
+
+    Object.keys(leafletMarkers.value).map(Number).forEach(markerId => {
+        const markedRemoved = !markerIds.includes(markerId)
+
+        if (markedRemoved) {
+            leafletMarkers.value[markerId].remove()
+            delete leafletMarkers.value[markerId]
+        }
+    })
+
+    markers.forEach(marker => {
+        const existingMarker = leafletMarkers.value[marker.id]
+
+        if (existingMarker) {
+            leafletMarkers.value[marker.id].setLatLng([marker.x, marker.y])
+        } else {
+            leafletMarkers.value[marker.id] = L.marker([marker.x, marker.y]).addTo(map.value)
+        }
+    })
+}
+watch(() => props.markers, (markers) => {
+    updateMarkers(markers)
+})
 
 onMounted(() => {
     map.value = L
@@ -32,10 +69,13 @@ onMounted(() => {
             // @ts-expect-error
             smoothWheelZoom: true,  // enable smooth zoom
             smoothSensitivity: 1.5,   // zoom speed. default is 1
+            attributionControl: false,
         })
         .fitBounds(floorPlanImageBounds)
         .setView([floorPlanImage.height / 2, floorPlanImage.width / 2], -1)
 
     L.imageOverlay(props.floorPlanUrl, floorPlanImageBounds).addTo(map.value)
+
+    updateMarkers(props.markers)
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,31 @@
 <template>
-    <div>
+    <div class="container mx-auto">
         <h1>Floor Map</h1>
-        <client-only>
-            <FloorMap ref="floorMap" id="floor-map" :markers="markers" :floorPlanUrl="floorPlanUrl" />
-        </client-only>
+        <ClientOnly>
+            <FloorMap ref="floorMapRef" id="floor-map" :markers="markers" :floorPlanUrl="floorPlanUrl" />
+        </ClientOnly>
+        <button class="bg-neutral-200 px-2 py-1 rounded mt-4" @click="zoomOnDwightDesk">
+            Zoom on Dwight's desk
+        </button>
     </div>
 </template>
 
 <script setup lang="ts">
-import FloorMap from "~/components/FloorMap.vue";
 
-const floorPlanUrl = '/office_floor.jpg'; // Path to the floor plan image
+const floorPlanUrl = '/office_floor.jpg';
 const markers = [
     {id: 1, x: 350, y: 1010, label: 'Michael'},
-    {id: 1, x: 430, y: 620, label: 'Jim'},
-    {id: 1, x: 530, y: 450, label: 'Dwight'},
-    {id: 1, x: 130, y: 520, label: 'Pam'},
+    {id: 2, x: 430, y: 620, label: 'Jim'},
+    {id: 3, x: 530, y: 450, label: 'Dwight'},
+    {id: 4, x: 130, y: 520, label: 'Pam'},
 ];
+
+const floorMapRef = ref();
+
+const zoomOnDwightDesk = () => {
+    const dwightMarker = markers[3]
+    floorMapRef.value?.zoomOnMarker(dwightMarker);
+}
 </script>
 
 <style scoped>
